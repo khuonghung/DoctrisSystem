68b5c7da6a7fdee7137331338219739adf67cc51
Khuong Hung <khuonghung1423@gmail.com>
2022-01-11 13:48:19 +0700

    Context : update connection database using azure cloud

--- Doctris/src/java/context/DBContext.java
+++ Doctris/src/java/context/DBContext.java
@@ -14,19 +14,13 @@ import java.sql.SQLException;
  * @author Khuong Hung
  */
 public class DBContext {
-    private final String condition = "allowPublicKeyRetrieval=true&verifyServerCertificate=false&useSSL=false&requireSSL=false";
 
     public Connection getConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String serverName = "localhost";
-            String portNumber = "3306";
-            String dbName = "DoctrisDB";
-            String url = "jdbc:mysql://" + serverName + ":" + portNumber + "/" + dbName + "?" + condition;
-
-            String username = "root";
-            String password = "142378956";
-
+            String url = "jdbc:mysql://clinicbooking.mysql.database.azure.com:3306/doctris_system";
+            String username = "khuonghung1423";
+            String password = "clinic.booking1423";
             Connection connection = DriverManager.getConnection(url, username, password);
             return connection;
         } catch (ClassNotFoundException | SQLException ex) {
@@ -34,8 +28,9 @@ public class DBContext {
         }
         return null;
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         DBContext db = new DBContext();
         System.out.println(db.getConnection());
+        System.out.println(db.getConnection().getCatalog());
     }
 }
