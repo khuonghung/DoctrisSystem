f0bdd3b11e8af492c6f932fb090a0076fa7ae59d
Khuong Hung <khuonghung1423@gmail.com>
2022-01-12 21:31:48 +0700

    Doctris: sync base code for iteration 1

---
+++ Doctris/src/java/dal/UserDAO.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dal;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import model.Account;
+import context.DBContext;
+/**
+ *
+ * @author Khuong Hung
+ */
+public class UserDAO {
+    
+    Connection conn = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+    
+}
