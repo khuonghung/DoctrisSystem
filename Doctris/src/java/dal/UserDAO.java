0234347cbe8548108caa28261fcc2b4109a0c62b
tuoidthe130958 <tuoidthe130958@fpt.edu.vn>
2022-01-25 00:16:10 +0700

    Doctris: Format code UserDao

--- Doctris/src/java/dal/UserDAO.java
+++ Doctris/src/java/dal/UserDAO.java
@@ -14,21 +14,22 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import model.Role;
+
 /**
  *
  * @author Khuong Hung
  */
 public class UserDAO {
-    
+
     PreparedStatement ps = null;
     ResultSet rs = null;
     DBContext dbc = new DBContext();
     Connection connection = null;
-    
+
     /**
      *
      * @param email
-     * @param password 
+     * @param password
      * @return
      * @throws java.sql.SQLException
      */
@@ -42,7 +43,7 @@ public class UserDAO {
             rs = ps.executeQuery();
             while (rs.next()) {
                 Role r = new Role(rs.getInt(2));
-                return new Account(rs.getString(1),r,rs.getString(3),rs.getString(4),rs.getBoolean(5),rs.getInt(6),rs.getString(7),rs.getString(8),rs.getBoolean(9));
+                return new Account(rs.getString(1), r, rs.getString(3), rs.getString(4), rs.getBoolean(5), rs.getInt(6), rs.getString(7), rs.getString(8), rs.getBoolean(9));
             }
         } catch (SQLException e) {
             System.out.println(e);
@@ -53,7 +54,7 @@ public class UserDAO {
         }
         return null;
     }
-    
+
     public Account checkAcc(String email, String username) throws SQLException {
         String sql = "select * from users where email=? or username=?";
         try {
@@ -64,7 +65,7 @@ public class UserDAO {
             rs = ps.executeQuery();
             while (rs.next()) {
                 Role r = new Role(rs.getInt(2));
-                return new Account(rs.getString(1),r,rs.getString(3),rs.getString(4),rs.getBoolean(5),rs.getInt(6),rs.getString(7),rs.getString(8),rs.getBoolean(9));
+                return new Account(rs.getString(1), r, rs.getString(3), rs.getString(4), rs.getBoolean(5), rs.getInt(6), rs.getString(7), rs.getString(8), rs.getBoolean(9));
             }
         } catch (Exception e) {
         } finally {
@@ -75,14 +76,14 @@ public class UserDAO {
         return null;
     }
 
-    public void Register(String email, String password, String username, int role_id,String name,int phone, boolean gender,String img,boolean status) throws SQLException {
+    public void Register(String email, String password, String username, int role_id, String name, int phone, boolean gender, String img, boolean status) throws SQLException {
         String sql = "INSERT INTO `doctris_system`.`users` (`username`, `role_id`, `password`, `name`, `gender`, `phone`, `email`,`img`,`status`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
         try {
             connection = dbc.getConnection();
             ps = connection.prepareStatement(sql);
             ps.setString(1, username);
-            ps.setInt(2,role_id);
-            ps.setString(3,password);
+            ps.setInt(2, role_id);
+            ps.setString(3, password);
             ps.setString(4, name);
             ps.setBoolean(5, gender);
             ps.setInt(6, phone);
@@ -97,14 +98,14 @@ public class UserDAO {
             }
         }
     }
-    
+
     public void Recover(String username, String password) throws SQLException {
         String sql = "UPDATE `doctris_system`.`users` SET `password` = ? WHERE (`username` = ?)";
         try {
             connection = dbc.getConnection();
             ps = connection.prepareStatement(sql);
             ps.setString(2, username);
-            ps.setString(1,password);
+            ps.setString(1, password);
             ps.executeUpdate();
         } catch (Exception e) {
         } finally {
@@ -113,8 +114,7 @@ public class UserDAO {
             }
         }
     }
-    
-    
+
     public Account checkAccByEmail(String email) throws SQLException {
         String sql = "select * from users where email=?";
         try {
@@ -124,7 +124,7 @@ public class UserDAO {
             rs = ps.executeQuery();
             while (rs.next()) {
                 Role r = new Role(rs.getInt(2));
-                return new Account(rs.getString(1),r,rs.getString(3),rs.getString(4),rs.getBoolean(5),rs.getInt(6),rs.getString(7),rs.getString(8),rs.getBoolean(9));
+                return new Account(rs.getString(1), r, rs.getString(3), rs.getString(4), rs.getBoolean(5), rs.getInt(6), rs.getString(7), rs.getString(8), rs.getBoolean(9));
             }
         } catch (Exception e) {
         } finally {
@@ -134,7 +134,7 @@ public class UserDAO {
         }
         return null;
     }
-    
+
     public List<Account> getAllAccount() throws SQLException {
         List<Account> list = new ArrayList<>();
         String sql = "SELECT u.username,u.name,u.gender,u.email,u.phone,r.name,u.status,u.img "
@@ -147,8 +147,7 @@ public class UserDAO {
             rs = ps.executeQuery();
             while (rs.next()) {
                 Role r = new Role(rs.getString(6));
-                list.add(new Account(rs.getString(1),r,rs.getString(2),rs.getBoolean(3),rs.getInt(5),rs.getString(4),rs.getString(8),rs.getBoolean(7)));
-                
+                list.add(new Account(rs.getString(1), r, rs.getString(2), rs.getBoolean(3), rs.getInt(5), rs.getString(4), rs.getString(8), rs.getBoolean(7)));
             }
         } catch (SQLException e) {
         } finally {
@@ -159,6 +158,6 @@ public class UserDAO {
         return list;
 
     }
-    
-    
+
+
 }
