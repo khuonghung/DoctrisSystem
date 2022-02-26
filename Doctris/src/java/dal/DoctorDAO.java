c5f28704ccff7974ae18ac5ae37f7e00b6a18ba3
Khuong Hung <khuonghung1423@gmail.com>
2022-02-26 23:45:33 +0700

    Doctris: Fix doctor information not showing the homepage

--- Doctris/src/java/dal/DoctorDAO.java
+++ Doctris/src/java/dal/DoctorDAO.java
@@ -37,7 +37,7 @@ public class DoctorDAO {
         String sql = "select concat_ws(cs.id,d.category_id)id,"
                 + " cs.name, cs.setting_id ,cs.status,"
                 + "d.doctor_id,d.role_id,d.doctor_name,d.username,"
-                + "d.gender,d.DOB,d.phone,d.description,d.status,d.img, d.fee"
+                + "d.gender,d.DOB,d.phone,d.description,d.status,d.img,d.fee "
                 + "from doctris_system.doctor d "
                 + "inner join doctris_system.category_service cs "
                 + "on d.category_id = cs.id ORDER BY RAND() LIMIT 8";
@@ -65,7 +65,7 @@ public class DoctorDAO {
                 }
                 Account a = new Account(rs.getString(8));
                 Setting s = new Setting(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getBoolean(4));
-                list.add(new Doctor(s, rs.getInt(5), rs.getInt(6), rs.getString(7), a, rs.getBoolean(9), rs.getDate(10), rs.getInt(11), rs.getString(12), rs.getBoolean(13), base64Image, rs.getDouble(15)));
+                list.add(new Doctor(s, rs.getInt(5), rs.getInt(6), rs.getString(7), a, rs.getBoolean(9), rs.getDate(10), rs.getInt(11), rs.getString(12), rs.getBoolean(13), base64Image,rs.getDouble(15)));
             }
         } catch (SQLException e) {
         } finally {
